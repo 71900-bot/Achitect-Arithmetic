028f4029b950b2b7cb23b3f70cdf4932e5f20f56
Viola Voon Li Wei <58481244+71900-bot@users.noreply.github.com>
2026-04-13 13:51:44 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+  class Program
+  {
+      // Task 1: Rectangles
+      static double React(double length, double width)
+      {
+        return length * width;
+      }
+
+      // Task 2: Circle
+      static double Circle(double radius)
+      {
+        return Math.PI * Math.Pow(radius, 2);
+      }
+
+      // Task 3: Triangle
+      static double Triangle(double bottom, double height)
+      {
+        return 0.5 * bottom * height;
+      }
+
+    public static void Main(string[] args)
+    {
+      // Simple tests from Task 4
+      Console.WriteLine(React(4, 5)); // 20
+      Console.WriteLine(Circle(4)); // ~50.265...
+      Console.WriteLine(Triangle(10, 9)); // 45
+    }
+  }
+}
+
